cc2d29258f787aefeb9e989e14bbba3a4daef9f0
kalipso <131648818+kalipsomasya@users.noreply.github.com>
2023-11-21 13:34:25 +0200

    1.

--- script.js
+++ script.js
@@ -1,5 +1,105 @@
-let a,b;
-a = prompt("Ievadi tekstu:");
-b= 280 - a.length
-alert("Teksts ir "+a.length+" simbolus garš. Vēl atlikusi vieta "+b+" no 280 simboliem");
-alert(a.slice(0, 280));
+// alert("meow");
+// console.log("meow");
+// document.write("meow");
+
+//const meow = "MEOW";
+//const dz_g = 2007;
+//let gd = 
+// alert(meow);
+// console.log(meow);
+// document.write(meow);
+
+// gd = 2023-dz_g;
+// console.log(gd);
+
+// let skait1, skait2, summa;
+// skait1= prompt("Ievadi 1 sk");
+// skait2= prompt("Ievadi 2 sk");
+// summa= skait1+ skait2
+// document.write(summa);
+
+
+// let skait11, skait22, summa1;
+// skait11= Number(prompt("Ievadi sk"));
+// skait22= Number(prompt("Ievadi sk2"));
+// summa1 = skait11+ skait22
+// alert(summa1);
+
+
+// //uzd1
+// let skait1, skait2, skait3, skait4, summa, substr, divis;
+// skait1= Number(prompt("Ievadi b"));
+// skait2= Number(prompt("Ievadi c"));
+// skait3= Number(prompt("Ievadi d"));
+// skait4= Number(prompt("Ievadi e"));
+// summa = (2*skait1)+skait2
+// substr = skait3-skait4
+// divis = summa/substr
+// alert("Rezultāts: "+divis)
+// alert(`Imzantotie skaitļi ${skait1}, ${skait2}, ${skait3}, ${skait4}.`)
+
+// //uzd2
+// let r, C, x;
+// r=Number(prompt("Ievadi riņķa līnijas radiusu:"));
+// x=Number(prompt("Ievadi mainīgo:"))
+// C= Math.round(2*Math.PI*r)
+// alert(C)
+// y= Math.pow(x, x);
+// z = Math.sqrt(y);
+// a= Math.abs(x-r);
+// alert(a)
+
+// let a,b;
+// b= Number(prompt("Ieraksti skaitli no 1 līdz 12: "));
+// a = (Math.floor((Math.random()*12)+1))
+// if(a===b){
+//    System.out.println( "Uzvarēji!")
+// }
+// else{
+//    System.out.println("Tu uzmeti "+a+ " un minētais skaitlis ir "+b)
+// }
+// alert("Tu uzmeti "+a+ " un minētais skaitlis ir "+b)
+
+for (let i =2; i<10; i=i+1) {
+   document.write("i");
+}
+
+for (let i =2; i<10; i++) {
+   document.write(i+ " ");
+}
+
+for (let i =2; i<10; i++) {
+   document.write(i+ '<br>');
+}
+
+for (let i =2; i<10; i=i+2) {
+   document.write(i+ " ");
+}
+
+// let beigas=10;
+// for (let i =1; i<beigas; i=i+2) {
+//     document.write(i+ " ");
+// }
+
+// let sakums=1;
+// let beigas=10;
+// for (let i=sakums; i<beigas; i++){
+//   document.write(i+ " ");
+// }
+// document.write('<br>');
+// document.write(" Izvadīju visus skaiļus no  " + sakums + "līdz " +(beigas-1));
+
+let sakums=2;
+let beigas=10;
+for (let i=sakums; i<=beigas; i++){
+  document.write(i +" kvadrātā ir "+i*i+'<br>');
+}
+
+for (let i=0; i<=4; i++){ 
+   document.write(i)
+};
+
+i="Pasaule"
+for (let x=0; x <= 10; x=x+1) {
+   document.write(i*x)
+}
