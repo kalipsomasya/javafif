135039bdfa9a30adf9ebf3c5f189ed7625238b7d
kalipso <131648818+kalipsomasya@users.noreply.github.com>
2023-12-08 17:08:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,63 @@
-// alert("meow");
-// console.log("meow");
-// document.write("meow");
+"use strict"
 
-//const meow = "MEOW";
-//const dz_g = 2007;
-//let gd = 
-// alert(meow);
-// console.log(meow);
-// document.write(meow);
-
-// gd = 2023-dz_g;
-// console.log(gd);
-
-// let skait1, skait2, summa;
-// skait1= prompt("Ievadi 1 sk");
-// skait2= prompt("Ievadi 2 sk");
-// summa= skait1+ skait2
-// document.write(summa);
+// const a=5
+// const b=6
+// document.write(a+b)
 
 
-// let skait11, skait22, summa1;
-// skait11= Number(prompt("Ievadi sk"));
-// skait22= Number(prompt("Ievadi sk2"));
-// summa1 = skait11+ skait22
-// alert(summa1);
+//funkcijas deklarešana
+// function izdruka(){
+//     document.write("mani sauc")
+// }
+
+// //izsauc funkciju
+// izdruka()
 
 
-// //uzd1
-// let skait1, skait2, skait3, skait4, summa, substr, divis;
-// skait1= Number(prompt("Ievadi b"));
-// skait2= Number(prompt("Ievadi c"));
-// skait3= Number(prompt("Ievadi d"));
-// skait4= Number(prompt("Ievadi e"));
-// summa = (2*skait1)+skait2
-// substr = skait3-skait4
-// divis = summa/substr
-// alert("Rezultāts: "+divis)
-// alert(`Imzantotie skaitļi ${skait1}, ${skait2}, ${skait3}, ${skait4}.`)
-
-// //uzd2
-// let r, C, x;
-// r=Number(prompt("Ievadi riņķa līnijas radiusu:"));
-// x=Number(prompt("Ievadi mainīgo:"))
-// C= Math.round(2*Math.PI*r)
-// alert(C)
-// y= Math.pow(x, x);
-// z = Math.sqrt(y);
-// a= Math.abs(x-r);
-// alert(a)
-
-let a,b;
-b= Number(prompt("Ieraksti skaitli no 1 līdz 12: "));
-a = (Math.floor((Math.random()*12)+1))
-if(a===b){
-   System.out.println( "Uzvarēji!")
+function sulus(ab, ap){
+    console.log(ab, ap)
+    const sula=`sula no ${ab} aboliem un ${ap} apelsiniem`
+    return sula
 }
-else{
-   System.out.println("Tu uzmeti "+a+ " un minētais skaitlis ir "+b)
+const absula = sulus(2,0)
+console.log(absula)
+
+const abapsula = sulus(2,4)
+console.log(abapsula)
+
+
+
+function perimetrs(a,b,c){
+    const per=`trijstura perimetrs ir ${a+b+c}`
+    return per
 }
-alert("Tu uzmeti "+a+ " un minētais skaitlis ir "+b)
+const tr = perimetrs(2,4,6)
+console.log(tr)
+
+//funkciju izteiksmes(function expression)
+function vecums(dzG){
+    const pV = 2023 - dzG
+    return pV
+}
+const vec = vecums(1986)
+console.log(vec)
+
+//izteiksme jeb anonima funkcija
+const vec1 = function(dzG){
+    return 2023 - dzG
+}
+console.log(vec1(2000))
+
+
+
+
+const tr1 = function(a,b,c){
+    return a+b+c
+}
+
+console.log(tr1(2,3,4))
+
+//bultu funkcijas (arrow function)
+const vec3 = dzG => 2023 - dzG
+const pilnG = vec3(1985)
+console.log(pilnG)
